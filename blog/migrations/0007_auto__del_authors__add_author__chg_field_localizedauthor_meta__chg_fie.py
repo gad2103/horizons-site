4ebe06ece090126912a509ee5ff2cb7f506a31c5
Gabriel Duncan <gad2103@gmail.com>
2013-07-08 05:19:49 -0400

    added new files. and migrations which i should take out

---
+++ blog/migrations/0007_auto__del_authors__add_author__chg_field_localizedauthor_meta__chg_fie.py
@@ -0,0 +1,141 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'Authors'
+        db.delete_table('blog_authors')
+
+        # Adding model 'Author'
+        db.create_table('blog_author', (
+            ('node_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['node.Node'], unique=True, primary_key=True)),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='+', null=True, to=orm['auth.User'])),
+            ('picture', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+        ))
+        db.send_create_signal('blog', ['Author'])
+
+
+        # Changing field 'LocalizedAuthor.meta'
+        db.alter_column('blog_localizedauthor', 'meta_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['blog.Author']))
+
+        # Changing field 'Blog.created_by'
+        db.alter_column('blog_blog', 'created_by_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['blog.Author'], null=True))
+
+    def backwards(self, orm):
+        # Adding model 'Authors'
+        db.create_table('blog_authors', (
+            ('picture', self.gf('django.db.models.fields.files.ImageField')(max_length=100, null=True, blank=True)),
+            ('user_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('node_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['node.Node'], unique=True, primary_key=True)),
+            ('author', self.gf('django.db.models.fields.related.ForeignKey')(related_name='+', null=True, to=orm['auth.User'], blank=True)),
+        ))
+        db.send_create_signal('blog', ['Authors'])
+
+        # Deleting model 'Author'
+        db.delete_table('blog_author')
+
+
+        # Changing field 'LocalizedAuthor.meta'
+        db.alter_column('blog_localizedauthor', 'meta_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['blog.Authors']))
+
+        # Changing field 'Blog.created_by'
+        db.alter_column('blog_blog', 'created_by_id', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['blog.Authors'], null=True))
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'blog.author': {
+            'Meta': {'object_name': 'Author', '_ormbases': ['node.Node']},
+            'author': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'+'", 'null': 'True', 'to': "orm['auth.User']"}),
+            'node_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.Node']", 'unique': 'True', 'primary_key': 'True'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'blog.blog': {
+            'Meta': {'object_name': 'Blog', '_ormbases': ['node.Node']},
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blog.Author']", 'null': 'True', 'blank': 'True'}),
+            'node_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.Node']", 'unique': 'True', 'primary_key': 'True'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'blog.localizedauthor': {
+            'Meta': {'object_name': 'LocalizedAuthor', '_ormbases': ['node.LocalizedNode']},
+            'description': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'localizednode_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.LocalizedNode']", 'unique': 'True', 'primary_key': 'True'}),
+            'meta': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blog.Author']"})
+        },
+        'blog.localizedblog': {
+            'Meta': {'object_name': 'LocalizedBlog', '_ormbases': ['node.LocalizedNode']},
+            'description': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'exerpt': ('django.db.models.fields.CharField', [], {'max_length': '511', 'null': 'True', 'blank': 'True'}),
+            'localizednode_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.LocalizedNode']", 'unique': 'True', 'primary_key': 'True'}),
+            'meta': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blog.Blog']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '63'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'node.datastate': {
+            'Meta': {'object_name': 'DataState'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'node.language': {
+            'Meta': {'object_name': 'Language'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'node.localizednode': {
+            'Meta': {'object_name': 'LocalizedNode', '_ormbases': ['node.Node']},
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'localization language'", 'to': "orm['node.Language']"}),
+            'node_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.Node']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'node.node': {
+            'Meta': {'object_name': 'Node'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'data_state': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['node.DataState']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'published': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 7, 6, 0, 0)', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['blog']
---
+++ blog/migrations/0008_auto__del_field_author_author.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting field 'Author.author'
+        db.delete_column('blog_author', 'author_id')
+
+
+    def backwards(self, orm):
+        # Adding field 'Author.author'
+        db.add_column('blog_author', 'author',
+                      self.gf('django.db.models.fields.related.ForeignKey')(related_name='+', null=True, to=orm['auth.User'], blank=True),
+                      keep_default=False)
+
+
+    models = {
+        'blog.author': {
+            'Meta': {'object_name': 'Author', '_ormbases': ['node.Node']},
+            'node_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.Node']", 'unique': 'True', 'primary_key': 'True'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'blog.blog': {
+            'Meta': {'object_name': 'Blog', '_ormbases': ['node.Node']},
+            'created_by': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blog.Author']", 'null': 'True', 'blank': 'True'}),
+            'node_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.Node']", 'unique': 'True', 'primary_key': 'True'}),
+            'picture': ('django.db.models.fields.files.ImageField', [], {'max_length': '100', 'null': 'True', 'blank': 'True'})
+        },
+        'blog.localizedauthor': {
+            'Meta': {'object_name': 'LocalizedAuthor', '_ormbases': ['node.LocalizedNode']},
+            'description': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '63'}),
+            'localizednode_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.LocalizedNode']", 'unique': 'True', 'primary_key': 'True'}),
+            'meta': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blog.Author']"})
+        },
+        'blog.localizedblog': {
+            'Meta': {'object_name': 'LocalizedBlog', '_ormbases': ['node.LocalizedNode']},
+            'description': ('tinymce.models.HTMLField', [], {'null': 'True', 'blank': 'True'}),
+            'exerpt': ('django.db.models.fields.CharField', [], {'max_length': '511', 'null': 'True', 'blank': 'True'}),
+            'localizednode_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.LocalizedNode']", 'unique': 'True', 'primary_key': 'True'}),
+            'meta': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['blog.Blog']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '63'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'node.datastate': {
+            'Meta': {'object_name': 'DataState'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'node.language': {
+            'Meta': {'object_name': 'Language'},
+            'code': ('django.db.models.fields.CharField', [], {'max_length': '10'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'})
+        },
+        'node.localizednode': {
+            'Meta': {'object_name': 'LocalizedNode', '_ormbases': ['node.Node']},
+            'language': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'localization language'", 'to': "orm['node.Language']"}),
+            'node_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['node.Node']", 'unique': 'True', 'primary_key': 'True'})
+        },
+        'node.node': {
+            'Meta': {'object_name': 'Node'},
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']", 'null': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {}),
+            'data_state': ('django.db.models.fields.related.ForeignKey', [], {'default': '1', 'to': "orm['node.DataState']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'modified': ('django.db.models.fields.DateTimeField', [], {}),
+            'published': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 7, 6, 0, 0)', 'null': 'True', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['blog']
